21bf8304fe2baa87b345d69c6be49643c7a171a8
Davi Fernandes <davi.fer159@gmail.com>
2025-05-08 12:08:37 -0300

    feat: add DOM elements

---
+++ src/formControl.ts
@@ -0,0 +1,6 @@
+const username = document.querySelector(".username") as HTMLInputElement;
+const email = document.querySelector(".email") as HTMLInputElement;
+const password = document.querySelector(".password") as HTMLInputElement;
+const confirmPassword = document.querySelector(
+  ".password2"
+) as HTMLInputElement;
