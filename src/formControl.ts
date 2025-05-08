293299628f2c538432f0fa8a632459d333cde4a8
Davi Fernandes <davi.fer159@gmail.com>
2025-05-08 12:59:36 -0300

    feat: add valitadion form

--- src/formControl.ts
+++ src/formControl.ts
@@ -1,6 +1,65 @@
+import isEmail from "validator/lib/isEmail";
+
 const username = document.querySelector(".username") as HTMLInputElement;
 const email = document.querySelector(".email") as HTMLInputElement;
 const password = document.querySelector(".password") as HTMLInputElement;
 const confirmPassword = document.querySelector(
   ".password2"
 ) as HTMLInputElement;
+const form = document.querySelector(".form") as HTMLFormElement;
+
+form.addEventListener("submit", function (event: Event) {
+  event.preventDefault();
+
+  hideErrorMessages(this);
+  checkFieldsIsEmpty(username, email, password, confirmPassword);
+  checkUsername(username);
+  checkEmailIsValid(email);
+  checkEqualPassword(password, confirmPassword);
+});
+
+function checkFieldsIsEmpty(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value) {
+      showErrorMessage(input, "Campo não pode ser vazio");
+    }
+  });
+}
+
+function checkUsername(input: HTMLInputElement): void {
+  if (input.value.length < 3) {
+    showErrorMessage(input, "Usuário deve ter 3 ou mais caracteres");
+  }
+}
+
+function checkEmailIsValid(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) {
+    showErrorMessage(input, "Email Inválido");
+  }
+}
+
+function checkEqualPassword(
+  password: HTMLInputElement,
+  confirmPassword: HTMLInputElement
+): void {
+  if (password.value !== confirmPassword.value) {
+    showErrorMessage(password, "Senhas devem ser iguais");
+    showErrorMessage(confirmPassword, "Senhas devem ser iguais");
+  }
+}
+
+function hideErrorMessages(form: HTMLFormElement) {
+  const formError = form.querySelectorAll(".error-message");
+
+  formError.forEach((err) => err.classList.remove("show-error-message"));
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentNode as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    ".error-message"
+  ) as HTMLSpanElement;
+
+  errorMessage.innerHTML = msg;
+  formFields.classList.add("show-error-message");
+}
